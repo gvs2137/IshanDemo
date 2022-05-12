20ace777a6c684f714df938d2a8fe33d651a129c
gvs2137 <singhgaurav2137@gmail.com>
2022-05-12 14:29:32 +0530

    1st for Ishan

---
+++ src/test/java/DRYRUNFORMAVEN1/NewMavenProject/class1Test.java
@@ -0,0 +1,18 @@
+package DRYRUNFORMAVEN1.NewMavenProject;
+
+import org.testng.annotations.Test;
+
+public class class1Test {
+	
+	@Test
+	public void Test1()
+	{
+		System.out.println("Testmethod1");
+	}
+	@Test
+	public void Test2()
+	{
+		System.out.println("Testmethod2");
+	}
+
+}
---
+++ src/test/java/DRYRUNFORMAVEN1/NewMavenProject/class2Test.java
@@ -0,0 +1,18 @@
+package DRYRUNFORMAVEN1.NewMavenProject;
+
+import org.testng.annotations.Test;
+
+public class class2Test {
+	
+	@Test
+	public void Test3()
+	{
+		System.out.println("Testmethod3");
+	}
+	@Test
+	public void Test4()
+	{
+		System.out.println("Testmethod4");
+	}
+
+}
